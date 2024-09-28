5e907dafc82792d9a9f19119afed30a60ab34776
DanielForero14 <danielforsa@unisabana.edu.co>
2024-09-27 22:43:29 -0500

    productos

---
+++ data/products.js
@@ -0,0 +1,8 @@
+const products = [
+    { id: '1', name: 'Producto 1', price: 100, category: 'Categoría 1' },
+    { id: '2', name: 'Producto 2', price: 200, category: 'Categoría 2' },
+    // Otros productos
+  ];
+  
+  module.exports = products;
+  
