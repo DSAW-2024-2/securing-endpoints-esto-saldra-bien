8c44546579bc5edd1ef634eef011ed624b43275c
DanielForero14 <danielforsa@unisabana.edu.co>
2024-09-27 22:43:03 -0500

    users

--- data/users.js
+++ data/users.js
@@ -1,9 +1,8 @@
 const users = [
     { id: '1', name: 'Admin', email: 'admin@admin.com', age: 30 },
+    { id: '2', name: 'John Doe', email: 'john@example.com', age: 25 },
     // Otros usuarios
-    
   ];
-
   
-
-
+  module.exports = users;
+  
