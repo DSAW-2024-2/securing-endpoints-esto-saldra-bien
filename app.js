802367d05585282c16532309ff2dc3440db3b16f
santibari <santiagobari@unisabana.edu.co>
2024-09-27 23:40:18 -0500

    inicio de despliegue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
